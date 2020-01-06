600081e8a04b193e72baaf19a83ab12aef468e78
Arthur Briginets <is.steel.97@gmail.com>
2020-01-06 23:02:18 +0200

    Updates

---
+++ precache-manifest.ae47f660b5ae514702abea0c901a5e80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b3860402511403a493f675e2b09ff14",
+    "url": "/nuchp-rsa/index.html"
+  },
+  {
+    "revision": "83133bbedf09a724d99a",
+    "url": "/nuchp-rsa/static/css/2.802837ff.chunk.css"
+  },
+  {
+    "revision": "4cf39023287dde5e63d3",
+    "url": "/nuchp-rsa/static/css/main.d440c737.chunk.css"
+  },
+  {
+    "revision": "83133bbedf09a724d99a",
+    "url": "/nuchp-rsa/static/js/2.ca394279.chunk.js"
+  },
+  {
+    "revision": "4cf39023287dde5e63d3",
+    "url": "/nuchp-rsa/static/js/main.a62ed75a.chunk.js"
+  },
+  {
+    "revision": "39b460ef6144eba533ae",
+    "url": "/nuchp-rsa/static/js/runtime-main.2a3ba2b3.js"
+  }
+]);
